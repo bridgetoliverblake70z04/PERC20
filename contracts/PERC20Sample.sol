e881e8400ce79b5d97b233a69dc0ad9d854006c3
bridgetoliverblake70z04 <bridgetoliverblake70z041@hotmail.com>
2024-09-17 02:48:30 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("edaad912b95c8219439430a52e2d50a5c0a3b8f813c55f74f46e87f914a980f4", "edaad912b95c8219439430a52e2d50a5c0a3b8f813c55f74f46e87f914a980f4") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
